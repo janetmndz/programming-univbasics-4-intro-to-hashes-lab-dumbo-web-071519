ca2924af7761cd3a4cceb77609ec7ef23d792af4
Janet Mendez <janetmendez.03@gmail.com>
2019-06-29 18:39:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,13 @@
 def new_hash
-  # return an empty hash
+  Hash.new
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  newHash = {:name => "Jane Doe"}
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {:name => 'Grace Hopper'}
 end
 
 def id_generator
